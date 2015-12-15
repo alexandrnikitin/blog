5bba420deba3c8a9ad6c5920d759df20c9475d96
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-12-15 22:15:06 +0200

    Continue

--- _drafts/hoisting-in-net-examples.md
+++ _drafts/hoisting-in-net-examples.md
@@ -10,45 +10,55 @@ comments: true
 share: true
 ---
 
-This post contents a bunch of examples of the hoisting optimization with assembly listings. It's based on the theoretical part ["Hoisting in .NET Explained"][post-part1]
+:warning: This post contains a bunch of examples of the JIT hoisting optimization with assembly listings. It's based on the theoretical part ["Hoisting in .NET Explained"][post-part1]
+
+### Preamble: How to get assembly code
+
+Windbg
+
+Library?
+
+BenchmarkDotNet
 
 ### Examples
 
-The first example will be a classic one. We access the array length property and an element in a loop.
+#### Array's length and element
+
+The first example will be a classic one. We access the array's length property and an element in a loop.
 
 ```csharp
-public int Method(int[] arr)
+public int Test(int[] arr)
 {
     var sum = 0;
     for (var i = 0; i < arr.Length; i++)
     {
-        sum += arr[0] + arr[i];
+        sum += arr[1] + arr[i];
     }
     return sum;
 }
 ```
 
 ```
-00007ffa`a05304f0 4883ec28        sub     rsp,28h
-00007ffa`a05304f4 33c0            xor     eax,eax
-00007ffa`a05304f6 33c9            xor     ecx,ecx
-00007ffa`a05304f8 448b4208        mov     r8d,dword ptr [rdx+8]
-00007ffa`a05304fc 4585c0          test    r8d,r8d
-00007ffa`a05304ff 7e1f            jle     00007ffa`a0530520
-00007ffa`a0530501 4183f800        cmp     r8d,0
-00007ffa`a0530505 761e            jbe     00007ffa`a0530525
-00007ffa`a0530507 448b4a10        mov     r9d,dword ptr [rdx+10h]
-00007ffa`a053050b 4103c1          add     eax,r9d
-00007ffa`a053050e 4c63d1          movsxd  r10,ecx
-00007ffa`a0530511 468b549210      mov     r10d,dword ptr [rdx+r10*4+10h]
-00007ffa`a0530516 4103c2          add     eax,r10d
-00007ffa`a0530519 ffc1            inc     ecx
-00007ffa`a053051b 443bc1          cmp     r8d,ecx
-00007ffa`a053051e 7feb            jg      00007ffa`a053050b
-00007ffa`a0530520 4883c428        add     rsp,28h
-00007ffa`a0530524 c3              ret
-00007ffa`a0530525 e8de14a95f      call    clr!TranslateSecurityAttributes+0x900d4 (00007ffa`fffc1a08) (JitHelp: CORINFO_HELP_RNGCHKFAIL)
-00007ffa`a053052a cc              int     3
+00007fff`23bb0580 4883ec28        sub     rsp,28h
+00007fff`23bb0584 33c0            xor     eax,eax
+00007fff`23bb0586 33c9            xor     ecx,ecx
+00007fff`23bb0588 448b4208        mov     r8d,dword ptr [rdx+8]
+00007fff`23bb058c 4585c0          test    r8d,r8d
+00007fff`23bb058f 7e1f            jle     00007fff`23bb05b0
+00007fff`23bb0591 4183f801        cmp     r8d,1
+00007fff`23bb0595 761e            jbe     00007fff`23bb05b5
+00007fff`23bb0597 448b4a14        mov     r9d,dword ptr [rdx+14h]
+00007fff`23bb059b 4103c1          add     eax,r9d
+00007fff`23bb059e 4c63d1          movsxd  r10,ecx
+00007fff`23bb05a1 468b549210      mov     r10d,dword ptr [rdx+r10*4+10h]
+00007fff`23bb05a6 4103c2          add     eax,r10d
+00007fff`23bb05a9 ffc1            inc     ecx
+00007fff`23bb05ab 443bc1          cmp     r8d,ecx
+00007fff`23bb05ae 7feb            jg      00007fff`23bb059b
+00007fff`23bb05b0 4883c428        add     rsp,28h
+00007fff`23bb05b4 c3              ret
+00007fff`23bb05b5 e84e14a85f      call    clr!TranslateSecurityAttributes+0x900d4 (00007fff`83631a08) (JitHelp: CORINFO_HELP_RNGCHKFAIL)
+00007fff`23bb05ba cc              int     3
 ```
 
 
--- _drafts/hoisting-in-net-examples/array-length.asm
+++ _drafts/hoisting-in-net-examples/array-length.asm
@@ -1,59 +1,38 @@
-0:005> !U 00007ffaa0425a58    
+0:003> !U 00007fff23bb0580 
 Normal JIT generated code
-HoistingInDotNetExamples.HoistingArray.Length(Int32[])
-Begin 00007ffaa05304f0, size 17
+HoistingInDotNetExamples.HoistingArray.Test(Int32[])
+Begin 00007fff23bb0580, size 3b
 *** WARNING: Unable to verify checksum for HoistingInDotNetExamples.exe
 
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 10:
-00007ffa`a05304f0 33c0            xor     eax,eax
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 10:
+>>> 00007fff`23bb0580 4883ec28        sub     rsp,28h
+00007fff`23bb0584 33c0            xor     eax,eax
 
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 12:
-00007ffa`a05304f2 33c9            xor     ecx,ecx
-00007ffa`a05304f4 8b5208          mov     edx,dword ptr [rdx+8]
-00007ffa`a05304f7 85d2            test    edx,edx
-00007ffa`a05304f9 7e0b            jle     00007ffa`a0530506
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 12:
+00007fff`23bb0586 33c9            xor     ecx,ecx
+00007fff`23bb0588 448b4208        mov     r8d,dword ptr [rdx+8]
+00007fff`23bb058c 4585c0          test    r8d,r8d
+00007fff`23bb058f 7e1f            jle     00007fff`23bb05b0
+00007fff`23bb0591 4183f801        cmp     r8d,1
+00007fff`23bb0595 761e            jbe     00007fff`23bb05b5
+00007fff`23bb0597 448b4a14        mov     r9d,dword ptr [rdx+14h]
 
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 14:
-00007ffa`a05304fb b801000000      mov     eax,1
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 14:
+00007fff`23bb059b 4103c1          add     eax,r9d
+00007fff`23bb059e 4c63d1          movsxd  r10,ecx
+00007fff`23bb05a1 468b549210      mov     r10d,dword ptr [rdx+r10*4+10h]
+00007fff`23bb05a6 4103c2          add     eax,r10d
 
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 12:
-00007ffa`a0530500 ffc1            inc     ecx
-00007ffa`a0530502 3bd1            cmp     edx,ecx
-00007ffa`a0530504 7ff5            jg      00007ffa`a05304fb
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 12:
+00007fff`23bb05a9 ffc1            inc     ecx
+00007fff`23bb05ab 443bc1          cmp     r8d,ecx
+00007fff`23bb05ae 7feb            jg      00007fff`23bb059b
 
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 17:
-00007ffa`a0530506 c3              ret
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 17:
+00007fff`23bb05b0 4883c428        add     rsp,28h
+00007fff`23bb05b4 c3              ret
 
-
-
-
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 10:
-00007ffa`a05304f0 4883ec28        sub     rsp,28h
-00007ffa`a05304f4 33c0            xor     eax,eax
-
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 12:
-00007ffa`a05304f6 33c9            xor     ecx,ecx
-00007ffa`a05304f8 448b4208        mov     r8d,dword ptr [rdx+8]
-00007ffa`a05304fc 4585c0          test    r8d,r8d
-00007ffa`a05304ff 7e1f            jle     00007ffa`a0530520
-00007ffa`a0530501 4183f800        cmp     r8d,0
-00007ffa`a0530505 761e            jbe     00007ffa`a0530525
-00007ffa`a0530507 448b4a10        mov     r9d,dword ptr [rdx+10h]
-
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 14:
-00007ffa`a053050b 4103c1          add     eax,r9d
-00007ffa`a053050e 4c63d1          movsxd  r10,ecx
-00007ffa`a0530511 468b549210      mov     r10d,dword ptr [rdx+r10*4+10h]
-00007ffa`a0530516 4103c2          add     eax,r10d
-
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 12:
-00007ffa`a0530519 ffc1            inc     ecx
-00007ffa`a053051b 443bc1          cmp     r8d,ecx
-00007ffa`a053051e 7feb            jg      00007ffa`a053050b
-
-C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\Array.cs @ 17:
-00007ffa`a0530520 4883c428        add     rsp,28h
-00007ffa`a0530524 c3              ret
-
-00007ffa`a0530525 e8de14a95f      call    clr!TranslateSecurityAttributes+0x900d4 (00007ffa`fffc1a08) (JitHelp: CORINFO_HELP_RNGCHKFAIL)
-00007ffa`a053052a cc              int     3
+C:\temp\HoistingInDotNetExamples\HoistingInDotNetExamples\HoistingArray.cs @ 10:
+*** ERROR: Symbol file could not be found.  Defaulted to export symbols for C:\Windows\Microsoft.NET\Framework64\v4.0.30319\clr.dll -
+00007fff`23bb05b5 e84e14a85f      call    clr!TranslateSecurityAttributes+0x900d4 (00007fff`83631a08) (JitHelp: CORINFO_HELP_RNGCHKFAIL)
+00007fff`23bb05ba cc              int     3
